1ee57ca896f4c2787031deda7c39a07dadda8442
Terminnus <terminnus95@gmail.com>
2025-06-12 18:32:42 -0600

    adding extras to the base files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+//    A command line tool that prints out the multiplication table of a number
+//    Copyright (C) 2025 Terminnus
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+use clap::Parser;
+
 fn main() {
     println!("Hello, world!");
 }
